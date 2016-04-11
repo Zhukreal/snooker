ae6b138e6347e4cf435905134fde191547798cd5
ezhuk <zhuk.evgeniy97@gmail.com>
2016-04-12 01:04:17 +0300

    Started with cookies

--- public/javascripts/controllers/mainCtrl.js
+++ public/javascripts/controllers/mainCtrl.js
@@ -1,6 +1,8 @@
 var app = angular.module('MainCtrl',['ngCookies']);
 
-app.controller('MainController', function($scope){
+app.controller('MainController', function($scope, $cookies){
     $scope.hello = "hello,creator!";
+    $cookies.put("name","e20r432");
     console.log($cookies.getAll());
+    console.log($cookies.get("connect.sid"));
 });
