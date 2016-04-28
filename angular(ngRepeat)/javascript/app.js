806a582e8d6b569f26f3cf6d5905a1d0b93acda1
echizhevskiy <egorkachic@gmail.com>
2016-04-28 20:03:34 +0200

    Changes id dynamic routing

--- angular(ngRepeat)/javascript/app.js
+++ angular(ngRepeat)/javascript/app.js
@@ -1,11 +1,14 @@
 var tablesApp = angular.module('tablesApp', ['ngRoute', 'ngResource']);
 
 tablesApp.config(function ($routeProvider) {
-    $routeProvider
-    .when('/tables', {
-      templateUrl: 'template/tables.html',
+
+   $routeProvider
+    .when('/table_'+'{{count}}', {
+      templateUrl: 'template/'+'{{count}}'+'.html',
       controller: 'tablesCtrl'
     })
+  
+   /* 
     .when('/table_1', {
       templateUrl: 'template/2.html',
       controller: 'first'
@@ -14,30 +17,48 @@ tablesApp.config(function ($routeProvider) {
       templateUrl: 'template/3.html',
       controller: 'second'
     })
-    // .when('/tables/:tableid', {
-    //   templateUrl: 'template/tablenumber.html',
-    //   controller: 'tablesRand'
+    // .when('/tables/table_2', {
+    //   templateUrl: 'template/3.html',
+    //   controller: 'second'
     // })
-    .otherwise({
-      redirectTo: '/'
-    });
+    .when('/tables/:tableid', {
+      templateUrl: 'template/tablenumber.html',
+      controller: 'tablesRand'
+    })*/
+  /*  .otherwise({
+      template: '<h1>404 no such page</h1>'
+     
+    });*/
 });
 
 tablesApp.controller('tablesCtrl', function($scope, $http){
-  $http.get('json/tables.json').success(function(data){
+ /* $http.get('json/tables.json').success(function(data){
     $scope.tables = data;
   });
 
   $scope.setImage = function(imageUrl){
     $scope.mainImageUrl = imageUrl;
-  }
+  }*/
 
+  $scope.maxIndex = 7;
+    $scope.count=1;
+
+    $scope.pagenext = function()
+    { 
+      if($scope.count<$scope.maxIndex)
+      $scope.count+=1;
+
+    }
+    $scope.pageprev = function()
+    {if($scope.count>1)
+      $scope.count-=1;
+    }
 });
 
-// tablesApp.controller('tablesRand', ['$scope', '$http', '$location', '$routeParams',  function($scope, $http, $location, $routeParams){
-//     $scope.tableid = $routeParams.tableid;
-// }]);
-//
+tablesApp.controller('tablesRand', ['$scope', '$http', '$location', '$routeParams',  function($scope, $http, $location, $routeParams){
+    $scope.tableid = $routeParams.tableid;
+}]);
+
 tablesApp.controller('first', ['$scope', '$http', '$location',  function($scope, $http, $location){
 
 }]);
