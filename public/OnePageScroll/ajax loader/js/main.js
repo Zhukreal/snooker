5556b606b220d61090718806b7666383dc4ddd8d
Ivan Panchenko <ivan97.minsk@gmail.com>
2016-05-04 15:23:20 +0300

    Creating ajax loader.

---
+++ public/OnePageScroll/ajax loader/js/main.js
@@ -0,0 +1,15 @@
+$(window).load(function() {
+	$("#loading").delay(2000).fadeOut(500);
+	$("#loading-center").click(function() {
+	$("#loading").fadeOut(500);
+	})
+})
+
+
+/*$("#loading").bind("ajaxSend", function(){
+	$(this).show(); 
+}).bind("ajaxComplete", function(){
+	$(this).hide(); 
+});
+*/
+
